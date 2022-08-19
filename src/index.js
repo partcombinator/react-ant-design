a9f391c88433a673872b2c13fca8e1a01fa9b99d
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2022-08-19 13:00:38 +0200

    sc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
